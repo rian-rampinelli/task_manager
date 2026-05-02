f52081a4e1426237da287ba2c53ec7501ab0ad66
Rian <riaann.barbosa@gmail.com>
2026-05-02 09:32:29 -0300

    criando dtos de users

--- src/main/java/com/rian/task_manager/user/dto/UserResponse.java
+++ src/main/java/com/rian/task_manager/user/dto/UserResponse.java
@@ -1,4 +1,24 @@
 package com.rian.task_manager.user.dto;
 
-public record UserResponse() {
+import com.rian.task_manager.user.User;
+
+import java.time.LocalDateTime;
+
+public record UserResponse(
+        Long id,
+        String name,
+        String email,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
+    public static UserResponse fromEntity(User user){
+        return new UserResponse(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getCreatedAt(),
+                user.getUpdatedAt()
+        );
+    }
+
 }
