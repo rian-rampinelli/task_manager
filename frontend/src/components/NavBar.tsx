96132e6a1abeb6ee04b81535b434527fc48c81d2
Rian <riaann.barbosa@gmail.com>
2026-05-23 16:37:41 -0300

    montando estrutura do react

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+function NavBar(){
+    return(
+        <div>
+            <h1>oi</h1>
+        </div>
+    )
+}
+
+export default NavBar
---
+++ frontend/src/pages/Home/HomePage.tsx
@@ -0,0 +1,6 @@
+import NavBar from '../../components/NavBar'
+function HomePage(){
+    return <NavBar></NavBar>
+}
+
+export default HomePage
