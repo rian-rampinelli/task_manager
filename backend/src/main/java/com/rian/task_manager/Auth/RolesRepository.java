2dfeff32cc4619bc7f2854045a81ad624d9f09c8
Rian <riaann.barbosa@gmail.com>
2026-06-27 02:03:53 -0300

    feat: add dependençias de jwt e configurando yaml

---
+++ backend/src/main/java/com/rian/task_manager/Auth/RolesRepository.java
@@ -0,0 +1,6 @@
+package com.rian.task_manager.Auth;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolesRepository extends JpaRepository<Roles, Long> {
+}
---
+++ backend/src/main/java/com/rian/task_manager/config/TokenProvider.java
@@ -0,0 +1,4 @@
+package com.rian.task_manager.config;
+
+public class TokenProvider {
+}
