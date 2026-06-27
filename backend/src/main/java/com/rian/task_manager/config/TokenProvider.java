af323dfa0e39c345b2aa1a551dc9a1af37f165cd
Rian <riaann.barbosa@gmail.com>
2026-06-27 02:10:48 -0300

    feat: injetando valores de yaml com @value no tokenProvider

--- backend/src/main/java/com/rian/task_manager/config/TokenProvider.java
+++ backend/src/main/java/com/rian/task_manager/config/TokenProvider.java
@@ -1,4 +1,16 @@
 package com.rian.task_manager.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TokenProvider {
+
+    @Value("${jwt.expiration}")
+    private Long expirationTime;
+
+    @Value("${jwt.key}")
+    private String key;
+
+
 }
